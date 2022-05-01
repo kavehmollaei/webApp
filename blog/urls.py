a239e650c1225beb7143049f67af3301f5f0daef
kavehmollaei <kavehmollaei@gmail.com>
2022-05-01 23:16:01 +0430

    add datails for posts in blog-item.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from website.views import index
-from blog.views import blog_single,blog_view,test,blog_details
+from blog.views import blog_single,blog_view
 
 
 
@@ -12,6 +12,6 @@ app_name='blog'
 urlpatterns = [
 path('',blog_view,name='index'),
 path('blog/single',blog_single,name='single'),
-path('<str:name>/<str:family_name>',test,name='test'),
-path('details/',blog_details,name='blog_details')
+path('<int:pid>',blog_single,name='blog_single'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from datetime import date
+from gc import get_objects
 import imp
 from multiprocessing.spawn import import_main_path
 import re
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from datetime import date
 from blog.models import Post
 
@@ -13,15 +14,14 @@ def blog_view(request):
     content={"posts": posts}
     return render(request,'blog/blog.html',context=content)
 
-def blog_single(request):
+def blog_single(request,pid):
+    post=get_object_or_404(Post,id=pid)
+    content={'post':post}
 
-    return render(request,'blog/blog-item.html')
+    return render(request,'blog/blog-item.html',context=content)
 
 def test(request,name,family_name):
     
     content={"firstname":name,"lastname":family_name}
     return render(request,'blog/test.html',context=content) 
 
-
-def blog_details(request):
-    return render(request,'blog/blog-item.html')
