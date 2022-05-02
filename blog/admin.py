6dd6f09892e48a316eaf253f29ec658a0516e976
kavehmollaei <kavehmollaei@gmail.com>
2022-05-02 15:55:25 +0430

    add author in admin pannel in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(admin.ModelAdmin):
    date_hierarchy='created_date'
    empty_value_display='-empty-'
     # fields=('title',)
-   list_display =('title','published_date','updated_time','counted_views','status','rate_views')
+   list_display =('title','author','published_date','updated_time','counted_views','status','rate_views')
 #    ordering=('updated_time',)
    list_filter = ('status',)
    search_fields=['content','title',]
